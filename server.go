b9dac256d6e594563fd7cb305d34aa122dae7014
Stickman008 <arm.supakorn@gmail.com>
2021-08-06 20:17:41 +0700

    Fix auth and Add tester

--- server.go
+++ server.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"gin-http-framework/controller"
+	"gin-http-framework/middlewares"
 	"gin-http-framework/service"
+	"io"
+	"os"
 
+	// gindump
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,8 +16,17 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
